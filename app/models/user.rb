a48b6444c9f1d614c6daee6bea54bc8086bc5f6b
tbs1117 <tbs19941117@gmail.com>
2021-09-04 17:56:30 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,17 @@ class User < ApplicationRecord
   # has_many :items
   # has_many :buyers
   
-  validates :nickname, presence: true
   validates :password,format:{with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}+\z/}
   with_options presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]+\z/} do
     validates :family_name
     validates :first_name
   end
-  with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/} do
-    validates :family_name_kana
-    validates :first_name_kana
-  end
-  validates :birthday, presence: true
+    with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/} do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+      with_options presence: true do
+        validates :birthday
+        validates :nickname
+      end
 end
