9be2e8d757406c5fd9221eb9dfadd5882c5f7088
tbs1117 <tbs19941117@gmail.com>
2021-09-04 12:03:30 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :buyers
+  # has_many :items
+  # has_many :buyers
   
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:7},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
   validates :birthday, presence: true
 end
