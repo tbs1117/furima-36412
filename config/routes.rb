bc4a1631168a04625515343512e3934d08cc2f45
tbs1117 <tbs19941117@gmail.com>
2021-09-07 17:45:53 +0900

    ビュー・ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :edit, :update]
+  resources :users, only: [:new, :create, :edit, :update]
+  resources :items, only: [:new, :create]
 end
