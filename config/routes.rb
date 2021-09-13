a3988952b0dc4ebae49ea0e1b59f5fe7ccdf804d
tbs1117 <tbs19941117@gmail.com>
2021-09-13 16:20:37 +0900

    ルーティング設定・遷移先等

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:new, :create]
-  resources :items, only: [:new, :create, :show, :edit, :update]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
 end
