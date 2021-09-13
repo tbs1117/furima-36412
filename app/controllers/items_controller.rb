fb511ec6799c5939578580c545576b7b47557f23
tbs1117 <tbs19941117@gmail.com>
2021-09-13 16:35:57 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_redirect, only: [:edit, :update]
+  before_action :set_redirect, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
