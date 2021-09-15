b7a133889fc1e1e510d93aa3a02d6736967d7783
tbs1117 <tbs19941117@gmail.com>
2021-09-15 11:37:08 +0900

    Formオブジェクト作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefectures_id, :city, :house_number, :building_name, :telephone_number, :user, :item
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :house_number
+    validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :user_id
+    validates :item_id
+  end
+  validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order_id)
+  end
+end
