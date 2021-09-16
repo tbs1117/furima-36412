dcdd4874e6100e29154426deb63302284622922c
tbs1117 <tbs19941117@gmail.com>
2021-09-16 21:12:15 +0900

    テストコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefectures_id, :city, :house_number, :building_name, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefectures_id, :city, :house_number, :building_name, :telephone_number, :user_id, :item_id, :token
+
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Include not hyphen(-)"}
-    validates :user_id
-    validates :item_id
+    validates :token
   end
   validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
-    Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  context '内容に問題がある場合' do
+    it "tokenが空では登録できないこと" do
+      @order.token = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Token can't be blank")
+    end
+  end
 end
