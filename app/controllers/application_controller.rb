0931e79c22ea9324e73fcb7dfcb401bc3f32ff09
tbs1117 <tbs19941117@gmail.com>
2021-09-18 12:49:13 +0900

    環境変数をRailsアプリケーション側で読み込む設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |nickname, password|
-      nickname == 'tsubasa' && password == '1117'
+      nickname == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
